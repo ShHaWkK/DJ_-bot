550f207b37b1b1507f6ef62481f7064bdb93c7ac
ShHaWkK <51519814+ShHaWkK@users.noreply.github.com>
2024-04-21 00:37:49 +0200

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,1 +1,15 @@
-skip.js
+module.exports = {
+    name: 'skip',
+    description: 'Skip to the next song in the queue',
+    execute(message, args, serverQueue) {
+        if (!message.member.voice.channel)
+            return message.channel.send("You need to be in a voice channel to skip music!");
+        if (!serverQueue)
+            return message.channel.send("There is no song that I could skip!");
+
+        // If there is a song that is currently playing
+        if (serverQueue.connection.dispatcher) {
+            serverQueue.connection.dispatcher.end();
+        }
+    },
+};
